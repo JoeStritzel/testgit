0a947f160234f47b41838c0f217555b30a57481f
JoeStritzel <strutzelj0709@warriormail.rlc.edu>
2014-10-29 09:07:27 -0500

    Update Form1.vb

--- testgit/Form1.vb
+++ testgit/Form1.vb
@@ -1,18 +1,6 @@
 ﻿Public Class Form1
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-
-    End Sub
-
-    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
-
-    End Sub
-
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         TextBox1.Text = "Whoooo"
     End Sub
+    
 End Class
